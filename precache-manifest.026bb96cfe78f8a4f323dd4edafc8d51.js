51e8875e5396e3738e7832ba55f5cc9285cb5269
Alex Sales <asales2005@gmail.com>
2019-12-21 19:41:14 -0800

    Updates

---
+++ precache-manifest.026bb96cfe78f8a4f323dd4edafc8d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad82a8c2ef9d3bf7abdb3cd24a06185",
+    "url": "/react-task-list-as/index.html"
+  },
+  {
+    "revision": "3c06ba03aa57552a3877",
+    "url": "/react-task-list-as/static/css/main.342b391a.chunk.css"
+  },
+  {
+    "revision": "278186ee5bdad462e0c9",
+    "url": "/react-task-list-as/static/js/2.8f4c7166.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-task-list-as/static/js/2.8f4c7166.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3c06ba03aa57552a3877",
+    "url": "/react-task-list-as/static/js/main.2c0d48a6.chunk.js"
+  },
+  {
+    "revision": "ec94f6c01912c30d789a",
+    "url": "/react-task-list-as/static/js/runtime-main.43a0ad52.js"
+  }
+]);
